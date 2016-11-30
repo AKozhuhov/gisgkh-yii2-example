aeee9f2f29269059d45d4c4f991782a626a95f94
korshunovmaxim <korshunov.m.e@gmail.com>
2016-11-30 14:40:07 +0500

    yandex metrika

--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -25,6 +25,36 @@ $this->title = ($this->title ?: 'Интеграция с ГИС ЖКХ с пом
     <?php $this->head() ?>
 </head>
 <body>
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter40269009 = new Ya.Metrika({
+                    id:40269009,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/40269009" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
 <?php $this->beginBody() ?>
 
 <div class="wrap">
